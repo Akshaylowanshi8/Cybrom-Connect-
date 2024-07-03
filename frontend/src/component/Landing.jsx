5c27a6caf8783fab99cdae6aa4ff400d54b03676
Santoshshriwas <santoshdhobi9589@gmail.com>
2024-07-03 15:47:00 +0530

    Footer Done and Landing Page

---
+++ frontend/src/component/Landing.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import img1 from "../assets/dbvmk0tsk0o0hd59fi64z3own.png";
+const Landing = () => {
+  return (
+   <>
+  <section>
+    <div className="container">
+      <div className="row">
+      <h1 className='let1'>Let the right people know you’re open to work</h1>
+      <h3 className='let2'>With the Open To Work feature, you can privately tell recruiters or publicly share with the LinkedIn community that you are looking for new job opportunities.</h3>
+    </div>
+    <div ><img className='img2' src={img1} alt="" /></div>
+    </div>
+
+    <div className='container1'>
+      <h1>Find the right job or internship for you</h1>
+      
+    </div>
+    
+    
+  </section>
+   </>
+  )
+}
+
+export default Landing
