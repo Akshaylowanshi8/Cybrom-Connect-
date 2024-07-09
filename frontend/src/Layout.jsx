4de6652ef6be7ace480a6ca43281d759bcd420e6
saurabh926 <saurabhrawat9826@gmail.com>
2024-07-09 15:15:02 +0530

    about page

--- frontend/src/Layout.jsx
+++ frontend/src/Layout.jsx
@@ -3,10 +3,6 @@ import Footer from "./component/Footer";
 import Navbar from "./Layout/Navbar";
 const Layout=()=>{
     return(<>
-<div className="bg-transparent shadow-sm  justify-evenly text-center  text-2xl align-middle flex play">
-<div className="">
-<img/>
-</div>
 <Navbar />
 <Outlet />
 <Footer />
