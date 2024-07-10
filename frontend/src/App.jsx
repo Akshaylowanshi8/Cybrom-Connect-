a8d8b8fcb8a7c20a1fe1683bacbc18f03be8de75
akshay <akshaylovanshi8@gmail.com>
2024-07-10 12:13:02 +0530

    done 2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,11 @@
 import './App.css'
 import React from 'react'
-import Footer from './component/Footer'
 import SignInForm from './component/SignInForm'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import UploadTech from './Dashboard/DashboardComponent/UploadTech'
 import About from './component/About'
 import Layout from './Layout'
 import Home from './Home';
-import SignInForm from './component/SignInForm';
 function App() {
   return (
 <BrowserRouter>
@@ -15,13 +13,9 @@ function App() {
   <Route path='/' element={<Layout />}>
   <Route path='home' element={<Home />} />
   <Route path='about' element={<About />} />
-  {/* <Route path='home' element={  <Landing />} /> */}
-  {/* <Route path='home' element={ } /> */}
   <Route path='login' element={ <SignInForm/>} />
   <Route path='login' element={ <SignInForm />} />
   </Route>
-
-
   <Route path='/dashboard' element={<Layout />}>
   {/* <Route path='dashboard' element={<Dashboard />} /> */}
   <Route path='/dashboard/uploadtech' element={ <UploadTech/>} />
@@ -30,6 +24,5 @@ function App() {
 </BrowserRouter>
   )
 }
-
 export default App
 
