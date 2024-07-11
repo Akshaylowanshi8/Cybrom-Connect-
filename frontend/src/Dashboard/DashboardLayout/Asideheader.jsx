b9c9bc4d979cbcbd2b9038d0ac196513d8de4a58
akshay <akshaylovanshi8@gmail.com>
2024-07-11 16:36:54 +0530

    merge

--- frontend/src/Dashboard/DashboardLayout/Asideheader.jsx
+++ frontend/src/Dashboard/DashboardLayout/Asideheader.jsx
@@ -1,28 +1,38 @@
 import React from "react";
-import { Link,Outlet } from "react-router-dom";
+import { Link,Outlet, useNavigate } from "react-router-dom";
 import "../Dashboard.assets/css/adminhome.css"
-import logo from "../Dashboard.assets/Img/dashlogo.jpg"
+
+
 const AdminHome = ()=>{
+  let mynav=useNavigate()
+const logout=()=>{
+alert("are you sour...")
+mynav("/home")
+
+  }
     return(
         <>
         <aside id="aside">
           <div id="aside-heading">
             <img 
-               src={logo} 
+               src="\src\assets\cclogos.png"
               alt="Logo" 
             />
+            <>
+
+            </>
             <h2>Admin</h2> 
             <hr style={{marginTop:"20px"}}/>
             <Link className="desh-link" to="uploadtech">uploadtech</Link>
             
           </div>
         </aside>
-        <nav id="nav">Hello
-          <div style={{position:"absolute",right:"50px"}}>
-            <Link className="desh-link" to="adminlogin" >Admin</Link>
+        <nav id="nav">
+          <div style={{position:"absolute",right:"50px"}} onClick={logout}  >
+          Logout
           </div>
-            
         </nav>
+        
         <Outlet />
       </>
     )
