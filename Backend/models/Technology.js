a569e7409a319174bf31d4a892accbda2d240647
himani bhimate <himanibhimate0808@gmail.com>
2024-07-12 13:28:48 +0530

    upload image array in cloudinary

--- Backend/models/Technology.js
+++ Backend/models/Technology.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 
 const TechSchema  = mongoose.Schema({
     Tname: String,
+    Taplydate:Date,
     Tdescription:String,
     Tprice:Number,
     Tvedio:String,
