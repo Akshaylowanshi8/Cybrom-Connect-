851366c30f59724c772b2e0fd50b60dcd972b886
Santoshshriwas <santoshdhobi9589@gmail.com>
2024-07-12 12:34:51 +0530

    Solve now

--- Backend/models/userModel.js
+++ Backend/models/userModel.js
@@ -1,12 +1,27 @@
+// const mongoose = require("mongoose");
+
+// const UserSchema  = mongoose.Schema({
+//     Uname: String,
+//     Unumber:Number,
+//     Uaddres:String,
+//     Ueducation:String,
+//     Uemail: { type: String, unique: true, required: true },
+//     Upass: { type: String,  unique: true, required: true },
+
+
+// })
+// module.exports = mongoose.model("user",UserSchema)
 const mongoose = require("mongoose");
 
 const UserSchema  = mongoose.Schema({
-    Uname: String,
-    Unumber:Number,
-    Uaddres:String,
-    Ueducation:String,
-    Uemail: { type: String, unique: true, required: true },
-    Upass: { type: String,  unique: true, required: true },
+    // Uname: String,
+    // // Unumber:Number,
+    // Uemail: { type: String, required: true },
+    // Upassword: { type: Number },
+
+    Uname: { type: String, required: true },
+    Uemail: { type: String, required: true },
+    Upassword: { type: String, required: true },
 
 
 })
--- Backend/server.js
+++ Backend/server.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+ require('dotenv').config()
 const express = require('express')
 const app = express()
 const bodyParser = require("body-parser");
@@ -6,13 +6,19 @@ const cors = require("cors");
 const port = 8000
 const mongoose = require("mongoose");
 const dashboardRoutes = require("./routes/dashboard.Router")
+const userRoutes = require("./routes/userRoute.js")
 mongoose
+
   .connect(process.env.DataBase)
-  .then(() => console.log("💻 Mondodb Connected"))
+  .then(() => console.log("💻 Mondodb Server Connected"))
+
   .catch(err => console.error(err));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
 // app.get('/', (req, res) => res.send('Hello World!'))
 app.use('/dashboard',dashboardRoutes)
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.use('/user',userRoutes)
+
+app.listen(port, () => console.log(`Server is Running Port ${port}!`))
+
